7a82e8a3e0f8697b94ebc753e592e2153c5145a7
seanlaughlin <71667909+seanlaughlin@users.noreply.github.com>
2023-05-04 15:43:06 +0100

    removed keys from gitignore temporarily

---
+++ config/keys.js
@@ -0,0 +1,16 @@
+//In gitignore to protect keys
+
+module.exports = {
+  google: {
+    clientID:
+      "327431342823-7td7sv6sk0q0a9vrnejittt64t50fhtv.apps.googleusercontent.com",
+    clientSecret: "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",
+  },
+  facebook: {
+    clientID: "256662076753179",
+    clientSecret: "dd3a4fe51b3902dc56446b14db297be7",
+  },
+  session: {
+    cookieKey: "sifbjgsdbfd333fee",
+  },
+};
